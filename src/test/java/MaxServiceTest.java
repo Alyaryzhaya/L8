c4e8c518d2eb890d4be8d06993c7f11946aff032
Natalia Nevzorova <alyaryzhaya@gmail.com>
2022-09-21 21:45:01 +0300

    initial commit

---
+++ src/test/java/MaxServiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MaxServiceTest {
+    @Test
+    public void shouldFindMax(){
+        MaxService service = new MaxService();
+
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+
+}
